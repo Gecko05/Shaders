bfa59273fadc633b0c63839be2f8162218558507
Jaime Centeno <jacacent@gmail.com>
2023-06-30 11:03:25 -0600

    Create 3rd.c

---
+++ 3rd.c
@@ -0,0 +1,23 @@
+															
+#ifdef GL_ES
+precision highp float;
+#endif
+
+uniform float time;
+uniform vec2  resolution;
+uniform float zoom;
+
+#define PI 3.14
+
+mat2 rotate3d(float angle)
+{
+    return mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
+}
+
+void main() {
+    vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
+    p *= rotate3d((time * 1.0) * PI);
+    float t;
+    t = 0.075 / abs(1.0/*sin(time)*/ - length(p));
+    gl_FragColor = vec4(vec3(t)  * vec3(0.20*(sin(time)+3.0), p.y*0.8, 3.0), 1.0);
+}
